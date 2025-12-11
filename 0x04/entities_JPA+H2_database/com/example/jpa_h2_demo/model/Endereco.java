984fea523c1f462155dc088985a901e681528e11
Roger Lopes <rogersouzasilva@hotmail.com>
2025-12-10 21:53:16 -0300

    refactor: Add JoinColumn annotation for Cliente relationship in Endereco and Telefone entities

--- 0x04/entities_JPA+H2_database/com/example/jpa_h2_demo/model/Endereco.java
+++ 0x04/entities_JPA+H2_database/com/example/jpa_h2_demo/model/Endereco.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 @Entity
@@ -17,6 +18,7 @@ public class Endereco {
         private String estado;
 
         @ManyToOne
+        @JoinColumn(name = "cliente_id")
         private Cliente cliente;
 
         public Endereco() {
--- 0x04/entities_JPA+H2_database/com/example/jpa_h2_demo/model/Telefone.java
+++ 0x04/entities_JPA+H2_database/com/example/jpa_h2_demo/model/Telefone.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 @Entity
@@ -16,6 +17,7 @@ public class Telefone {
         private String tipo;
 
         @ManyToOne
+        @JoinColumn(name = "cliente_id")
         private Cliente cliente;
 
         public Telefone() {
